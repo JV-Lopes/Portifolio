a1a9a87c63e8c7c4bba90acfceb5885dd84607ff
José Vieira Lopes Neto <160689405+JV-Lopes@users.noreply.github.com>
2024-05-31 14:01:06 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -14,3 +14,15 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var contatoBtn = document.querySelector('.Contato');
+    if (window.scrollY > 50) {
+      header.classList.add('scroll');
+      contatoBtn.classList.add('scroll');
+    } else {
+      header.classList.remove('scroll');
+      contatoBtn.classList.remove('scroll');
+    }
+  });
