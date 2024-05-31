b3fcb7fc44198bd9a1d8f6ed901a7f9b1d83a1ee
José Vieira Lopes Neto <160689405+JV-Lopes@users.noreply.github.com>
2024-05-31 00:37:41 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -14,22 +14,3 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var celular = document.getElementById('celular').value;
-    var mensagem = document.getElementById('mensagem').value;
-    var feedback = document.getElementById('feedback');
-
-    if (nome === "" || email === "" || mensagem === "") {
-        feedback.textContent = "Por favor, preencha todos os campos obrigatórios.";
-        feedback.style.color = "red";
-    } else {
-        feedback.textContent = "Mensagem enviada com sucesso!";
-        feedback.style.color = "green";
-        
-        document.getElementById('contactForm').reset();
-    }
-});
